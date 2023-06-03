999a1d2954f826672020a735fde3c3d36d1b58af
Jose Jorge Montes Otero <Jose.Montes@jala.university>
2023-06-03 16:14:43 -0500

    secciones de productos mas vendidos y mejor valorados

---
+++ public/scripts/sectionsProductsBest.js
@@ -0,0 +1,35 @@
+import { Get } from "../controller/UserApi.js";
+
+const products = document.querySelectorAll("#BestProduct")
+
+
+Get("products?_sort=likes&_order=desc").then((productsDB)=>{
+    for(var i = 0; i<5;i++){
+        const childrens = products[i].children;
+        const producturl = productsDB[i].imgurl;
+        const productname = productsDB[i].name;
+        const productprice = productsDB[i].price;
+        const productId = productsDB[i].id
+        childrens[0].src = producturl
+        childrens[1].textContent = productname
+        childrens[2].textContent = productprice + "$" 
+        products[i].addEventListener("click",()=>{
+            localStorage.setItem(`producto-${productId}`, productId)
+        })
+    }
+})
+
+//? para el carrito obtener la id de los productos que el user dio click
+
+export const getidproductsaves = ()=>{
+    // Obtén todas las keys almacenadas en el local storage
+    const keys = Object.keys(localStorage);
+
+    // Filtra las keys que corresponden a las IDs de los productos
+    const ids = keys.filter(key => key.startsWith('producto-'));
+
+    // Obtén los valores correspondientes a las IDs filtradas
+    const idValues = ids.map(id => localStorage.getItem(id));
+
+    console.log(idValues);
+}
