d9cf47c50c0d77bfc584df4f6b5be5256ee9b0ba
Jhoseb <Jose.Moya@jala.university>
2023-06-12 14:37:10 -0500

    cambios del register, firstname, lastname, documento y cambios en los terminos y condiciones

--- public/scripts/loginAnimation.js
+++ public/scripts/loginAnimation.js
@@ -1,3 +1,2 @@
 particlesJS.load('particles-js', '../assets/particlesjs-config.json', function(){
-  console.log('El fondo animado ha sido cargado');
 })
--- public/scripts/register.js
+++ public/scripts/register.js
@@ -1,34 +1,30 @@
 import { RegisterUser } from "../controller/UserController.js";
 import { validateConfirmPass } from "./validationFormRegister.js";
+
 const registerForm = document.getElementById('registerForm');
 
+registerForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+  const username = document.getElementById('name');
+  const email = document.getElementById('email');
+  const password = document.getElementById('password');
+  const confirmPassword = document.getElementById('confirmPassword');
+  const firstname = document.getElementById('firstname'); 
+  const lastname = document.getElementById('lastname'); 
+  const docu = document.getElementById('document'); 
 
+  if (password.value !== confirmPassword.value) {
+    validateConfirmPass(password, confirmPassword);
+    return; // Detener la ejecución si no coinciden
+  }
 
-
-
-registerForm.addEventListener('submit', function (event){
-    
-    event.preventDefault();
-    const username = document.getElementById('name')
-    const email = document.getElementById('email')
-    const password = document.getElementById('password')
-    const confirmPassword = document.getElementById('confirmPassword');
-
-    if (password.value !== confirmPassword.value) {
-        validateConfirmPass(password,confirmPassword)
-        return; // Detener la ejecución si no coinciden
-    }
-    
-    RegisterUser(username, email, password.value)
+  RegisterUser(username, email, password.value, firstname.value, lastname.value, docu.value)
     .then(function () {
-        // Realizar acciones adicionales después del registro exitoso
-        window.location.href = '../views/login.html'
+      // Realizar acciones adicionales después del registro exitoso
+      window.location.href = '../views/login.html';
     })
-    .catch(function (error) {
-        
-        document.getElementById('name').value = '';
-        document.getElementById('email').value = '';
+    .catch(function () {
+      document.getElementById('name').value = '';
+      document.getElementById('email').value = '';
     });
-
-})
-
+});
