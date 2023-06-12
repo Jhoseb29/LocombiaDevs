8f37b38e0ba88e18ee545633bc9123f8a7dd11ce
Alejandro Valero <alejandro.valero@jala.university>
2023-06-11 22:27:06 -0500

    Last changes. Removed Update product button, empty lines, corrected list separation lines.

--- public/scripts/adminList.js
+++ public/scripts/adminList.js
@@ -1,16 +1,13 @@
 import { Get } from "../controller/UserApi.js"
-
 const totalHistoric = document.getElementById('list')
-
 Get('products').then(products => {
     products.forEach(entry => {
-        const price = entry.price; // Obtains the price of the first product
-        const gameName = entry.name; // Obtains the name of the first game
-        const stock = entry.stock; // Obtains the stock xde
+        const price = entry.price;
+        const gameName = entry.name;
+        const stock = entry.stock;
         const brand = entry.brand;
 
         const historicContainer = document.createElement('div')
-
         const productName = document.createElement('h2')
         productName.textContent = gameName
         const productPrice = document.createElement('p')
@@ -24,10 +21,9 @@ Get('products').then(products => {
 
         historicContainer.appendChild(productName)
         historicContainer.appendChild(productPrice)
-        historicContainer.appendChild(productLine)
         historicContainer.appendChild(productStock)
         historicContainer.appendChild(productBrand)
+        historicContainer.appendChild(productLine)
         totalHistoric.appendChild(historicContainer)
-
     });
 });
